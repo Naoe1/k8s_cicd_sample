b7ff4131f72e2dea497d79f4782bc4a4b385d6b6
Gabriel Naoe <gllnaoe@gmail.com>
2026-03-12 13:32:55 +0800

    bump API version to 1.0.4 in welcome message

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   res.json({
     message: "Welcome to the Product API",
-    version: "1.0.3",
+    version: "1.0.4",
     endpoints: {
       getAllProducts: "GET /api/products",
       getProductById: "GET /api/products/:id",
